cc5e1b997e9283a836f83dd6daedbe451632d14d
cemonel <cemonel97@gmail.com>
2018-01-13 23:21:34 +0300

    Venue detail page added

--- client/urls.py
+++ client/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.search_page, name='search_page'),
+    url(r'^venue_detail/(?P<venue_id>[a-zA-Z0-9]+)/$', views.venue_detail, name='venue_detail'),
 ]
--- client/views.py
+++ client/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect
 from .forms import SearchForm
 from .models import Search
@@ -15,7 +16,6 @@ def search_page(request):
 		parameters = {
 			'near': location,
 			'query': food,
-			'limit': 10,
 			'oauth_token': 'BIH2RYEL3G20JYPXJX4LNZ01EL3VTMC0QXDNOTZKE5NZRAJL',
 			'v': '20180111'
 		}
@@ -28,7 +28,8 @@ def search_page(request):
 
 		for venue in venues:
 			venue_info = {}
-			venue_info['name'] = venue.get("name", "---")
+			venue_info['id'] = venue.get('id')
+			venue_info['name'] = venue.get('name', "---")
 			venue_info['phone'] = venue['contact'].get("formattedPone", "---")
 			venue_info['checkin_count'] = venue['stats'].get('checkinsCount', "---")
 			venues_list.append(venue_info)
@@ -36,12 +37,58 @@ def search_page(request):
 		venues_list = []
 		form = SearchForm()
 
+	page = request.GET.get('page', 1)  # pagination
+	paginator = Paginator(venues_list, 5)
+	try:
+		venues_list = paginator.page(page)
+	except PageNotAnInteger:
+		venues_list = paginator.page(1)
+	except EmptyPage:
+		venues_list = paginator.page(paginator.num_pages)
+
 	if Search.objects.count() > 5:
 		previous_searches = Search.objects.order_by()[Search.objects.count() - 5:]
-		previous_searches = previous_searches[::-1]  # reverses list
-	else:
-		previous_searches = Search.objects.order_by()[::-1]  # reverses list
+
+	previous_searches = previous_searches[::-1]  # reverses list
 
 	return render(request, 'search_page.html', context={'form': form,
 														'venues_list': venues_list,
 														'pre_searches': previous_searches})
+
+
+def venue_detail(request, venue_id):
+	parameters = {
+		'oauth_token': 'BIH2RYEL3G20JYPXJX4LNZ01EL3VTMC0QXDNOTZKE5NZRAJL',
+		'v': '20180111'
+	}
+	url_params = urllib.parse.urlencode(parameters)
+	request_url = 'https://api.foursquare.com/v2/venues/' + venue_id + '?' + url_params
+	response = requests.get(request_url)
+	response = response.json()
+	venue_detail = {}
+	venue_detail['name'] = response['response']['venue']['name']
+	venue_detail['phone'] = response['response']['venue']['contact'].get("phone", "-")
+	venue_detail['twitter'] = response['response']['venue']['contact'].get("twitter", "-")
+	venue_detail['facebook'] = response['response']['venue']['contact'].get("facebook", "-")
+	venue_detail['icon'] = response['response']['venue']['categories'][0]['icon'].get('prefix', 'default') + "bg_32.png"
+	page = request.GET.get('page', 1)  # pagination
+	offset = 1 + 5 * (int(page)-1)
+	next_page_number = str(int(page) + 1)
+	previous_page_number = str(int(page) - 1)
+	parameters = {
+		'oauth_token': 'BIH2RYEL3G20JYPXJX4LNZ01EL3VTMC0QXDNOTZKE5NZRAJL',
+		'v': '20180111',
+		'limit': '5',
+		'offset': offset,
+	}
+
+	url_params = urllib.parse.urlencode(parameters)
+	request_url = 'https://api.foursquare.com/v2/venues/' + venue_id + "/tips?" + url_params
+	response = requests.get(request_url)
+	response = response.json()
+	venue_tips = response['response']['tips']["items"]
+
+	return render(request, 'venue_detail.html', context={'venue_detail': venue_detail, 'venue_tips': venue_tips,
+														 'page': page,
+														 'next_page_number': next_page_number,
+														 'previous_page_number': previous_page_number})
